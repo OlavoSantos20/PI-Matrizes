67703189facb241ecd8384c8a2dda2a72259df50
OlavoSantos20 <olavosantos546@gmail.com>
2026-01-22 19:44:50 +0000

    chore: cache

--- Projeto Interdisciplinar/Projeto Interdisciplinar/obj/Debug/net8.0/Projeto Interdisciplinar.AssemblyInfo.cs
+++ Projeto Interdisciplinar/Projeto Interdisciplinar/obj/Debug/net8.0/Projeto Interdisciplinar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto Interdisciplinar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+997036b66870fa777d04cd3ac3bbe4ee4b230af5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbbdca3591529b3396bc07d0cab76edad1d3500c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto Interdisciplinar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto Interdisciplinar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
